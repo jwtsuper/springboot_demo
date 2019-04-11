23da6a07e14e88e27fbb45c9ea06b3115d393a11
jwtsuper <49516997+jwtsuper@users.noreply.github.com>
2019-04-11 19:47:32 +0800

    Update Second.java

--- src/main/java/com/baizhi/conf/Second.java
+++ src/main/java/com/baizhi/conf/Second.java
@@ -2,6 +2,6 @@ package com.baizhi.conf;
 
 public class Second {
     public static void main(String[] args) {
-        System.out.println("hahahahah");
+        System.out.println("you seccessed");
     }
 }
